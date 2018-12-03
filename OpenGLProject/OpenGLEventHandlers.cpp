3a27ed59d9032ba9325e0957c270a907bc9e77ef
Aidar Yakupov <yakupov.aidar.ru@yandex.ru>
2018-12-03 18:08:11 +0500

    mouse click -> draw Sierpinski

--- OpenGLProject/OpenGLEventHandlers.cpp
+++ OpenGLProject/OpenGLEventHandlers.cpp
@@ -1,24 +1,91 @@
 #include "OpenGLEventHandlers.h"
-#include "Point.h"
 #include "Math.h"
 
-OpenGLEventHandlers::OpenGLEventHandlers()
-{
+OpenGLEventHandlers::OpenGLEventHandlers() {
 }
 
-OpenGLEventHandlers::~OpenGLEventHandlers()
-{
+OpenGLEventHandlers::~OpenGLEventHandlers() {
 }
 
 void __cdecl OpenGLEventHandlers::display() {	
+	_clearScreen();
+}
+
+void __cdecl OpenGLEventHandlers::reshape(int width, int height) {
+}
+
+int OpenGLEventHandlers::currentPoint = 0;
+Point OpenGLEventHandlers::points[3];
+
+void __cdecl OpenGLEventHandlers::_clearScreen() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+
+void __cdecl OpenGLEventHandlers::_drawPoint(Point point) {
+	glColor3f(1, 1, 1);
+
+	glBegin(GL_POINTS);
+		glVertex2i(point.x, point.y);
+	glEnd();
+
+	glFlush();
+}
+
+void __cdecl OpenGLEventHandlers::mouse(int button, int state, int x, int y) {
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		if (currentPoint == 3) {
+			currentPoint = 0;
+			_clearScreen();
+		}
+		Point point = Point(x, 600 - y);
+		_drawPoint(point);		
+
+		points[currentPoint] = point;
+		currentPoint++;	
+	}
+	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+		currentPoint = 0;
+		_clearScreen();
+	}
+	if (currentPoint == 3) {
+		_sierpinski(points[0], points[1], points[2]);
+	}
+}
+
+void __cdecl OpenGLEventHandlers::keyboard(unsigned char key, int x, int y) {
+}
+
+void OpenGLEventHandlers::_triangle(Point point1, Point point2, Point point3) {
+	glBegin(GL_TRIANGLES);
+
+	glVertex2i(point1.x, point1.y);
+	glVertex2i(point2.x, point2.y);
+	glVertex2i(point3.x, point3.y);
+
+	glEnd();
+
+	glFlush();
+}
+
+void OpenGLEventHandlers::_quad(Point point1, Point point2, Point point3, Point point4){
+	glBegin(GL_QUADS);
+
+	glVertex2i(point1.x, point1.y);
+	glVertex2i(point2.x, point2.y);
+	glVertex2i(point3.x, point3.y);
+	glVertex2i(point4.x, point4.y);
+
+	glEnd();
+
+	glFlush();
+}
+
+void OpenGLEventHandlers::_sierpinski(Point point1, Point point2, Point point3) {
 	Math math;
-
-	glClear(GL_COLOR_BUFFER_BIT);	
-
-	glPointSize(2);
-	glColor3f(1.0, 1.0, 0);
-
-	Point triangle[3] = {Point(0, 0), Point(400, 600), Point(800, 0)};
+	
+	Point triangle[3] = {point1, point2, point3};
 	Point point = triangle[math.Random(3)];
 
 	glBegin(GL_POINTS);
@@ -41,14 +108,4 @@ void __cdecl OpenGLEventHandlers::display() {
 	glEnd();
 
 	glFlush();
-}
-
-void __cdecl OpenGLEventHandlers::reshape(int width, int height) {
-	
-}
-
-void __cdecl OpenGLEventHandlers::mouse(int button, int state, int x, int y) {
-}
-
-void __cdecl OpenGLEventHandlers::keyboard(unsigned char key, int x, int y) {
 }
--- OpenGLProject/OpenGLEventHandlers.h
+++ OpenGLProject/OpenGLEventHandlers.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "OpenGL.h"
+#include "Point.h"
 
 class OpenGLEventHandlers
 {
@@ -10,5 +11,15 @@ public:
 	static void __cdecl display();
 	static void __cdecl reshape(int width, int height);
 	static void __cdecl mouse(int button, int state, int x, int y);
-	static void __cdecl keyboard(unsigned char key, int x, int y);
+	static void __cdecl keyboard(unsigned char key, int x, int y);	
+
+private:
+	static int currentPoint;
+	static Point points[3];
+
+	static void _clearScreen();
+	static void _drawPoint(Point point);
+	static void _sierpinski(Point point1, Point point2, Point point3);
+	static void _triangle(Point point1, Point point2, Point point3);
+	static void _quad(Point point1, Point point2, Point point3, Point point4);
 };
